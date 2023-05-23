608529ec8d18957b24e63bbe0a2e2a432294a5f6
JanarthJ <janarthananjegadesan@gmail.com>
2023-05-23 22:58:26 +0530

    sample test

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,19 @@
 import React,{Component} from 'react';
 import './header.css'; 
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 export default  class Header extends Component {  
+
+  async funct(e){
+    e.preventDefault();
+    try {
+      const response = await axios.get('http://3.109.203.162:8080/api/getenquiry');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   render() {      
     return(
       <div className={'heads'}>
@@ -14,8 +25,9 @@ export default  class Header extends Component {
           <Link to="about">About</Link> |{" "}
           {/* <Link to="about">Product</Link> |{" "} */}
           <Link to="partners">Partners</Link> |{" "}
-          <Link to="contact">Contact Us</Link>
+          {/* <Link to="contact">Contact Us</Link> */}
         </nav>
+        <button onClick={(e)=>this.funct(e)}>click me</button>
         </div>       
       </div>
     ) 
